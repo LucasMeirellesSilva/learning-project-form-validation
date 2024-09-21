98bcbc0e9d58394d446ba870c437787e5acc2582
Lucas Kauan Meirelles da Silva <107893278+LucasMeirellesSilva@users.noreply.github.com>
2024-09-21 19:01:35 -0300

    checkName checkSurname

--- main.js
+++ main.js
@@ -1,22 +1,115 @@
 document.getElementById('formulario').addEventListener('submit', function(event) {
-    event.preventDefault();
-    checkName()
-  })
+  event.preventDefault();
+  removerTodosAlertas();
+  checkName();
+  checkSurname();
+});
 
-const alerta = document.createElement("img")
-alerta.setAttribute('src', './icones/Alert-icon.png')
-alerta.setAttribute('alt', "Ícone de Alerta")
-alerta.style.cssText = "width: 32px;"
+function criarAlerta() {
+  const alerta = document.createElement("img");
+  alerta.classList.add('alerta');
+  alerta.setAttribute('src', './icones/Alert-icon.png');
+  alerta.setAttribute('alt', "Ícone de Alerta");
+  alerta.setAttribute('title', '');
+  alerta.style.cssText = "width: 24px;";
+  return alerta;
+}
 
-// const form = document.getElementById("formulario")
-// form.append(alerta)
+// Função que remove alertas ao usuário editar um campo de texto específico.
+function removerAlerta(e) {
+  const inputParent = e.target.closest('.input-usuario');
+  const alertaImg = inputParent.querySelector('img.alerta');
+  if (alertaImg) {
+      alertaImg.remove();
+  }
+  const divToRemove = inputParent.querySelector('div.alerta');
+  if (divToRemove) {
+    divToRemove.replaceWith(...divToRemove.childNodes);
+  }
+  e.target.classList.remove('input-alerta')
+}
+
+// Função que remove todos os alertas para impedir que eles acumulem a cada tentativa de envio do formulário.
+function removerTodosAlertas() {
+  const alertas = document.querySelectorAll('img.alerta');
+  if (alertas) {
+      alertas.forEach(alerta => {
+          alerta.remove();
+      })
+  }
+}
+
+function criarDiv() {
+  const div = document.createElement('div');
+  div.classList.add('alerta');
+  div.style.cssText = 'display: flex; justify-content: space-between; align-items: center;';
+  return div;
+}
+
+function contemNumeros(valor) {
+  const numeros = [...Array(10).keys()];
+  const contemNumero = numeros.some(num => valor.includes(num));
+  return contemNumero;
+}
 
 function checkName() {
-    const nome = document.getElementById('nome')
-    valor = String(nome.value)
-    if (valor.lenght <= 2)
-        alerta.setAttribute('title', 'O nome precisa ter mais que dois caractéres.')
-        nome.parentNode.insertBefore(alerta, nome)
+  const alerta = criarAlerta();
+  const nome = document.getElementById('nome');
+  const userText = String(nome.value);
+  nome.addEventListener('change', removerAlerta);
+  const div = criarDiv();
+  const label = nome.parentElement.querySelector('label');
+
+  function appendAlert() {
+    div.appendChild(label);
+    div.appendChild(alerta);
+    nome.parentNode.insertBefore(div, nome);
+    nome.classList.add('input-alerta');
+    nome.focus();
+  }
+  
+  if (userText.length <= 2) {
+      alerta.title += 'O nome precisa ter mais que dois caractéres.';
+      appendAlert();
+  }
+  if (userText.length > 50) {
+      alerta.title += 'O nome não pode ter mais que cinquenta caractéres.';
+      appendAlert();
+  } 
+  if (contemNumeros(userText)) {
+      alerta.title += '\nO nome não pode incluir números.';
+      appendAlert();
+  }
+}
+
+function checkSurname() {
+  const alerta = criarAlerta();
+  const sobrenome = document.getElementById('sobrenome');
+  const userText = String(sobrenome.value);
+  sobrenome.addEventListener('change', removerAlerta);
+  const div = criarDiv();
+  const label = sobrenome.parentElement.querySelector('label');
+
+  function appendAlert() {
+    div.appendChild(label);
+    div.appendChild(alerta);
+    sobrenome.parentNode.insertBefore(div, sobrenome);
+    sobrenome.classList.add('input-alerta');
+    sobrenome.focus();
+  }
+  
+  if (userText.length <= 2) {
+      alerta.title += 'O sobrenome precisa ter mais que dois caractéres.';
+      appendAlert();
+  } 
+  else if (userText.length > 50) {
+      alerta.title += 'O sobrenome não pode ter mais que cinquenta caractéres.';
+      appendAlert();
+  } 
+  else if (contemNumeros(userText)) {
+      alerta.title += '\nO sobrenome não pode incluir números.';
+      appendAlert();
+  }
 }
 
 // function checkData() {
