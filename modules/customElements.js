5572a489b25d9cc5dc35a18d67faeedd63ff1db5
aatticuss <arturbr2004@gmail.com>
2024-10-03 00:42:06 -0300

    fix geral e comentarios

--- modules/customElements.js
+++ modules/customElements.js
@@ -1,3 +1,6 @@
+// Função para customização dos input radio
+// Esconde o input e marca ele como ativo ao clicar na div que encapsula ele
+// aplica um efeito de profundidade com a classe checked
 export function toggleRadio(element){
     const radios = document.querySelectorAll('input[name="experiencia"]');
     radios.forEach((radio) => {
@@ -9,10 +12,13 @@ export function toggleRadio(element){
     element.classList.add('checked');
 }
 
+// Função para customização dos input checkbox
+// Esconde o input e marca ele como ativo ao clicar na div que encapsula ele
+// aplica um efeito de profundidade com a classe checked
 export function toggleCheckbox(element){
-    let checkbox = element.querySelector('.box');
+    const checkbox = element.querySelector('.box');
     checkbox.checked = !checkbox.checked;
     element.classList.toggle('checked', checkbox.checked);
-    let conteudo = element.querySelector('.language-content')
+    const conteudo = element.querySelector('.language-content')
     conteudo.classList.toggle('checked', checkbox.checked);
 };
